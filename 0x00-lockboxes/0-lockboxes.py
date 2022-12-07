7624b1246544d2a53dca253dfebc2d5a2fe8f097
KristiSeraj <serajkristi@gmail.com>
2022-12-07 04:27:43 -0800

    lockbox

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    """Method that determines if all the boxes can be opened"""
+    new_list = [0] 
+    for key, value in enumerate(boxes):
+        for a_key in value:
+            if a_key < len(boxes) and a_key not in new_list and a_key != key:
+                new_list.append(a_key)
+    return len(new_list) == len(boxes)

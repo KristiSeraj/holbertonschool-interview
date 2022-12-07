5419fac11a421ceab5909fa5125eb865a288661d
KristiSeraj <serajkristi@gmail.com>
2022-12-07 04:43:36 -0800

    new method created by vivian

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python3
 """Unlock module"""
 
+
 def canUnlockAll(boxes):
     """Method that determines if all the boxes can be opened"""
-    new_list = [0]
-    for key, value in enumerate(boxes):
-        for a_key in value:
-            if a_key < len(boxes) and a_key not in new_list and a_key != key:
-                new_list.append(a_key)
+    new_list = [x for x in boxes[0]]
+    ls = [0]
+
+    for i in range(len(boxes)):
+        if i in new_list:
+            for j in range(len(boxes[i])):
+                if boxes[i][j] not in new_list:
+                    new_list.append(boxes[i][j])
+                    if boxes[i][j] < i:
+                        for n in boxes[boxes[i][j]]:
+                            if boxes[n] not in new_list:
+                                ls.append(n)
+    for i in ls:
+        new_list.append(i)
+    new_list = set(new_list)
     return len(new_list) == len(boxes)

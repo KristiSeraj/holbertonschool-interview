88448e36969104a15fd4bf18cf291d3d7032f5ed
KristiSeraj <serajkristi@gmail.com>
2023-01-28 07:47:09 -0800

    test

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import sys
+import re
+from collections import Counter
+
+total_size = 0
+status_codes = Counter()
+
+pattern = re.compile(r"(\d+\.\d+\.\d+\.\d+) - \[.+\] \"GET /projects/\d+ HTTP/1\.1\" (\d+) (\d+)")
+
+try:
+    for i, line in enumerate(sys.stdin):
+        match = pattern.match(line)
+        if not match:  # skipping invalid lines
+            continue
+        _, status_code, file_size = match.groups()
+        total_size += int(file_size)
+        status_codes[status_code] += 1
+
+        if (i + 1) % 10 == 0:
+            print("File size:", total_size)
+            for code in sorted(status_codes.keys()):
+                print(f"{code}: {status_codes[code]}")
+
+except KeyboardInterrupt:
+    print("File size:", total_size)
+    for code in sorted(status_codes.keys()):
+        print(f"{code}: {status_codes[code]}")

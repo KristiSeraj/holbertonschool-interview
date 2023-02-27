a0607d0b8b3b0e91756bb851a7d6549f9b352991
KristiSeraj <serajkristi@gmail.com>
2023-02-27 09:45:30 -0800

    fix

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -1,47 +1,68 @@
 #include "slide_line.h"
+void merge(int *line, int move, int i, int start,
+	int *current, int *current_idx, int *blank)
+{
 
+	if (line[i] == 0)
+	{
+		*blank += 1;
+	}
+
+	if (i == start && line[i] != 0)
+	{
+		*current = line[i];
+		*current_idx = i;
+	}
+
+	if (line[i] != 0 && line[i] == *current && i != start)
+	{
+		line[*current_idx] += line[i];
+		line[i] = 0;
+		*current = 0;
+		*blank += 1;
+	}
+
+	if (line[i] != 0 && *current != line[i] && i != start)
+	{
+		line[i - (*blank * move)] = line[i];
+		*current = line[i];
+		*current_idx = i - (*blank * move);
+		if (*blank)
+			line[i] = 0;
+	}
+}
 int slide_line(int *line, size_t size, int direction)
 {
-	int *place_here = NULL;
-	int *left = NULL;
-	int *right = NULL;
 
-	if (direction == SLIDE_LEFT)
+	int i, move, current, current_idx, blank, start;
+	int sizeX = (int) size;
+
+	if (!line || (direction != 0 && direction != 1))
+		return (0);
+
+	current = 0;
+	blank = 0;
+
+	if (direction == 0)
+		move = 1;
+	else
+		move = -1;
+
+	if (direction == 0)
 	{
-		place_here = line;
-		left = place_here;
+		for (i = start = current_idx =  0; i < sizeX; i++)
+		{
+			merge(line, move, i, start,
+				&current, &current_idx, &blank);
 
-		while (left < line + (size - 1))
-		{
-			while (*left == 0 && left < line + (size - 1))
-			{
-				left++;
-			}
-			right = left + 1;
-			while (right < line + (size))
-			{
-				if (*right == *left)
-				{
-					*place_here = *left * 2;
-					if (place_here != left)
-					{
-						*left = 0;
-					}
-					*right = 0;
-					place_here++;
-					break;
-				}
-				else
-				{
-					right++;
-				}
-			}
-			left++;
 		}
-		if (*(line + size - 1) && !*place_here)
+	}
+	else
+	{
+		for (i = start = current_idx = sizeX - 1; i >= 0; i--)
 		{
-			*place_here = *(line + size - 1);
-			*(line + size - 1) = 0;
+			merge(line, move, i, start,
+				&current, &current_idx, &blank);
 		}
 	}
 	return (1);

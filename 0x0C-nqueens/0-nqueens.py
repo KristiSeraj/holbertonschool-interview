0c6e9bb6d1dd1e5d862d5e6b35f7a057c0e5ffa9
KristiSeraj <serajkristi@gmail.com>
2023-05-29 19:43:14 +0200

    fix

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -17,10 +17,7 @@ class NQueen:
         or if the are attacking queens in row or diagonal (False)
         """
 
-        # j checks from 1 to k - 1 (Up to previous queen)
         for j in range(1, k):
-            # There is already a queen in column
-            # or a queen in same diagonal
             if self.x[j] == i or \
                abs(self.x[j] - i) == abs(j - k):
                 return 0
@@ -31,19 +28,15 @@ class NQueen:
         Args:
         k: starting queen from which to evaluate (should be 1)
         """
-        # i goes from column 1 to column n (1st column is 1st index)
         for i in range(1, self.n + 1):
             if self.place(k, i):
-                # Queen can be placed in i column
                 self.x[k] = i
                 if k == self.n:
-                    # Placed all 4 Queens (A solution was found)
                     solution = []
                     for i in range(1, self.n + 1):
                         solution.append([i - 1, self.x[i] - 1])
                     self.res.append(solution)
                 else:
-                    # Need to place more Queens
                     self.nQueen(k + 1)
         return self.res
 

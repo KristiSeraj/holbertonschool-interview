6ae02b62c54a294de6f86171c07ef878336c4d92
KristiSeraj <serajkristi@gmail.com>
2023-06-18 14:44:30 +0200

    fix

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -1,52 +1,47 @@
 #include "search.h"
-
 /**
  * linear_skip - searches for a value in a sorted skip list of integers
- * @list: pointer to the head of the skip list to search in
- * @value: value to search for
- *
- * Return: Pointer on the first node where value is located or NULL
- */
+ * @list:  pointer to the head of the skip list to search in
+ * @value: the value to search for
+ * Return: a pointer on the first node where value is located, otherwise NULL
+ **/
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-	size_t i;
-	skiplist_t *node_pos, *node_min;
+	skiplist_t *_list;
 
 	if (!list)
 		return (NULL);
-
-	node_pos = list, node_min = list;
-
-	while (node_pos && node_pos->next && (node_pos->n) < value)
+	_list = list;
+	while (_list->express)
 	{
-		node_min = node_pos;
-
-		if (node_pos->express)
+		printf("Value checked at index [%lu] = [%d]\n",
+			_list->express->index, _list->express->n);
+		if (_list->express->n >= value)
 		{
-			node_pos = node_pos->express;
-
-			printf("Value checked at index [%lu] = [%d]\n",
-				   node_pos->index, node_pos->n);
+			printf("Value found between indexes [%lu] and [%lu]\n",
+				_list->index, _list->express->index);
+			break;
 		}
-		else
-			while (node_pos->next)
-				node_pos = node_pos->next;
+		_list = _list->express;
 	}
-
-	printf("Value found between indexes [%lu] and [%lu]\n",
-		   node_min->index, node_pos->index);
-
-	for (i = node_min->index;
-		 i <= (node_pos->index) && (node_min->n <= value);
-		 i++, node_min = node_min->next)
+	if (!_list->express)
 	{
-		printf("Value checked at index [%lu] = [%d]\n", i, node_min->n);
-		if (node_min && (node_min->n) == value)
-			return (node_min);
+		list = _list;
+		while (list->next)
+			list = list->next;
+		printf("Value found between indexes [%lu] and [%lu]\n",
+			_list->index, list->index);
 	}
-
-	if (node_min)
-		printf("Value checked at index [%lu] = [%d]\n", i, node_min->n);
-
+	list = _list;
+	while (list != _list->express)
+	{
+		printf("Value checked at index [%lu] = [%d]\n",
+			list->index, list->n);
+		if (list->n == value)
+			break;
+		list = list->next;
+	}
+	if (list != _list->express)
+		return (list);
 	return (NULL);
 }

c3f441f5d8f8820412218b465ffe9ee131f2e83c
KristiSeraj <serajkristi@gmail.com>
2023-06-27 10:52:30 +0200

    fix

--- 0x1A-double_circular_linked_list/0-add_node.c
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -4,57 +4,67 @@
  * @str: string
  * Return: Address of the new node, or NULL on failure
 */
-List *create_node(char *str) {
+List *create_node(char *str)
+{
     List *new_node = malloc(sizeof(List));
-    if (new_node == NULL) {
-        return NULL;
+    if (new_node == NULL)
+    {
+        return (NULL);
     }
     new_node->str = strdup(str);
     new_node->prev = NULL;
     new_node->next = NULL;
-    return new_node;
+    return (new_node);
 }
-
 /**
  * add_node_end - Add a new node to the end of a double circular linked list
  * @list: list
  * @str: string
  * Return: Address of the new node, or NULL on failure
 */
-List *add_node_end(List **list, char *str) {
+List *add_node_end(List **list, char *str)
+{
     List *new_node = create_node(str);
-    if (new_node == NULL) {
-        return NULL;
+    if (new_node == NULL)
+    {
+        return (NULL);
     }
-    if (*list == NULL) {
+    if (*list == NULL)
+    {
         new_node->prev = new_node;
         new_node->next = new_node;
         *list = new_node;
-    } else {
+    }
+    else
+    {
         List *last_node = (*list)->prev;
         new_node->prev = last_node;
         new_node->next = *list;
         last_node->next = new_node;
         (*list)->prev = new_node;
     }
-    return new_node;
+    return (new_node);
 }
-
 /**
  * add_node_begin - Add a new node to the beginning of a double circular linked list
  * @list: list
  * @str: string
  * Return: Address of the new node, or NULL on failure
 */
-List *add_node_begin(List **list, char *str) {
+List *add_node_begin(List **list, char *str)
+{
     List *new_node = create_node(str);
-    if (new_node == NULL) {
-        return NULL;
+    if (new_node == NULL)
+    {
+        return (NULL);
     }
-    if (*list == NULL) {
+    if (*list == NULL)
+    {
         new_node->prev = new_node;
         new_node->next = new_node;
-    } else {
+    }
+    else
+    {
         List *first_node = *list;
         List *last_node = (*list)->prev;
         new_node->prev = last_node;
@@ -63,5 +73,6 @@ List *add_node_begin(List **list, char *str) {
         last_node->next = new_node;
     }
     *list = new_node;
-    return new_node;
+    
+    return (new_node);
 }
